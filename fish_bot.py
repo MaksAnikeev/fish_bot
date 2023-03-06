f50bc8bdc0699a7bee3c80f6c6225a3d95baf37d
MaksAnikeev <anikeev.maks@rambler.ru>
2023-03-06 07:55:55 +0300

    Launch store

--- fish_bot.py
+++ fish_bot.py
@@ -1,60 +1,137 @@
 import environs
 import requests
 from pprint import pprint
-
-env = environs.Env()
-env.read_env()
-
-client_id = env.str("CLIENT_ID")
-client_secret = env.str("CLIENT_SECRET")
-
-# data = {
-#     'client_id': client_id,
-#     'client_secret': client_secret,
-#     'grant_type': 'client_credentials',
-# }
-#
-# response = requests.post('https://api.moltin.com/oauth/access_token', data=data)
-#
-# pprint(response.json())
-
-access_token = env.str("ACCESS_TOKEN_BEARER")
-
-# headers = {
-#     'Authorization': f'Bearer {access_token}',
-#     'Content-Type': 'application/json',
-# }
-#
-# json_data = {
-#     'data': {
-#         'id': '23d0e2f9-2234-49bc-a132-cf9c3828fec5',
-#         'type': 'cart_item',
-#         'quantity': 8,
-#             }
-#         }
-#
-# response = requests.post('https://api.moltin.com/v2/carts/abc/items', headers=headers, json=json_data)
-# pprint(response.json())
-
 import os
 import logging
 import redis
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Filters, Updater
 from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
+from more_itertools import chunked
+from functools import partial
 
 _database = None
 
 
+def get_token():
+    client_id = env.str("CLIENT_ID")
+    client_secret = env.str("CLIENT_SECRET")
+
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials',
+    }
+    response = requests.post('https://api.moltin.com/oauth/access_token', data=data)
+    access_token = response.json()['access_token']
+    return access_token
+
+
+def get_products_params(access_token):
+    headers = {
+            'Authorization': f'Bearer {access_token}',
+        }
+    response = requests.get('https://api.moltin.com/pcm/products', headers=headers)
+    return response.json()
+
+
+def get_product_params(access_token, product_id):
+    headers = {
+            'Authorization': f'Bearer {access_token}',
+        }
+    params = {
+        'include': 'component_products',
+    }
+    response = requests.get(f'https://api.moltin.com/pcm/products/{product_id}',
+                            headers=headers,
+                            params=params)
+    return response.json()
+
+
+def get_products_names(products_params):
+    keyboard_products = [InlineKeyboardButton("Главное меню", callback_data='main_menu')]
+    for product in products_params['data']:
+        button_name = product['attributes']['name']
+        button_id = product['id']
+        button = InlineKeyboardButton(button_name, callback_data=button_id)
+        keyboard_products.insert(0, button)
+    return keyboard_products
+
+
 def start(update, context):
-    update.message.reply_text(text='Привет!')
-    return "ECHO"
+    keyboard = [[InlineKeyboardButton("Магазин", callback_data='store'),
+                 InlineKeyboardButton("Моя корзина", callback_data='cart')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    try:
+        update.message.reply_text('Привет! Сделай выбор:', reply_markup=reply_markup)
+        return 'MAIN_MENU'
+    except:
+        query = update.callback_query
+        context.bot.edit_message_text(text='Привет! Сделай выбор:',
+                                      chat_id=query.message.chat_id,
+                                      message_id=query.message.message_id,
+                                      reply_markup=reply_markup)
+        return 'MAIN_MENU'
 
 
-def echo(update, context):
-    users_reply = update.message.text
-    update.message.reply_text(users_reply)
-    return "ECHO"
+def send_products_keyboard(update, context, products_names):
+    query = update.callback_query
+    keyboard = list(chunked(products_names, 2))
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.edit_message_text(text='Выбери товар из магазина:',
+                                  chat_id=query.message.chat_id,
+                                  message_id=query.message.message_id,
+                                  reply_markup=reply_markup)
+    return "STORE"
+
+
+def button(update, context):
+    query = update.callback_query
+
+    if query.data == 'store':
+        products_names = dispatcher.bot_data['products_names']
+        return send_products_keyboard(update, context, products_names)
+
+    elif query.data == 'cart':
+        keyboard = [[InlineKeyboardButton("Главное меню", callback_data='main_menu')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        context.bot.edit_message_text(
+            text="Моя корзина",
+            chat_id=query.message.chat_id,
+            message_id=query.message.message_id,
+            reply_markup=reply_markup)
+        return 'CART'
+
+    elif query.data == 'main_menu':
+        return start(update, context)
+
+    elif query.data == 'back':
+        products_names = dispatcher.bot_data['products_names']
+        send_products_keyboard(update, context, products_names)
+        return "DESCRIPTION"
+
+    else:
+        keyboard = [[InlineKeyboardButton("Назад", callback_data='back')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        product_id = query.data
+        access_token = env.str("ACCESS_TOKEN_BEARER")
+        product_params = get_product_params(access_token, product_id)
+        if product_params['errors'][0]['status'] == 401:
+            access_token = get_token()
+            product_params = get_product_params(access_token, product_id)
+        pprint(product_params)
+
+        context.bot.edit_message_text(
+            text=f"Selected option: {query.data}",
+            chat_id=query.message.chat_id,
+            message_id=query.message.message_id,
+            reply_markup=reply_markup)
+        return "PRODUCT"
+
 
 
 def handle_users_reply(update, context):
@@ -76,7 +153,11 @@ def handle_users_reply(update, context):
 
     states_functions = {
         'START': start,
-        'ECHO': echo
+        'MAIN_MENU': button,
+        'STORE': button,
+        "PRODUCT": button,
+        'CART': button,
+        "DESCRIPTION": button,
     }
     state_handler = states_functions[user_state]
     try:
@@ -100,11 +181,29 @@ def get_database_connection():
     return _database
 
 
+
 if __name__ == '__main__':
+    env = environs.Env()
+    env.read_env()
+    access_token = env.str("ACCESS_TOKEN_BEARER")
+    products_params = get_products_params(access_token)
+    if products_params['errors'][0]['status'] == 401:
+        access_token = get_token()
+        products_params = get_products_params(access_token)
+
+    products_names = get_products_names(products_params)
+
     token = env.str("TG_BOT_TOKEN")
     updater = Updater(token)
     dispatcher = updater.dispatcher
+
+    dispatcher.bot_data['products_names'] = products_names
+
     dispatcher.add_handler(CommandHandler('start', handle_users_reply))
     dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
     dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
+
     updater.start_polling()
+    updater.idle()
